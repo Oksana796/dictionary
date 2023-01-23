690babf0c32401f479b3851ccb81a01eec6ffc36
Oksana <ksenia79460@gmail.com>
2023-01-23 12:05:58 +0000

    font-size

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,26 +2,24 @@ import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
 
-import '../styles/Result.css'
+import "../styles/Result.css";
 
 function Result(props) {
   if (props.results) {
-    
-        return (
-        <div className="result">
-          <section>
-            <h2>{props.results.word}</h2>
-            <Phonetic phone={props.results.phonetics} />
-          </section>
-          {props.results.meanings.map(function (meaning, index) {
-            return (
-              <section key={index}>
-                <Meaning mean={meaning} />
-              </section>
-            );
-          })}
-        </div>
-       
+    return (
+      <div className="result">
+        <section>
+          <h2>{props.results.word}</h2>
+          <Phonetic phone={props.results.phonetics} />
+        </section>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <section key={index}>
+              <Meaning mean={meaning} />
+            </section>
+          );
+        })}
+      </div>
     );
   } else {
     return null;
